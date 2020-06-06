9ddb0574518c43fb2d8a82e1ae531ccb722db9ae
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-06-06 22:36:05 +0530

    merging sheets - 1st

--- helper.py
+++ helper.py
@@ -25,6 +25,25 @@ def append_rows(self, values, value_input_option='RAW'):
     }
     return self.spreadsheet.values_append(self.title, params, body)
 
+def get_sheet(sheet):
+    scope = [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file'
+    ]
+    file_name = 'client_key.json'
+    creds = ServiceAccountCredentials.from_json_keyfile_name(file_name,scope)
+    client = gspread.authorize(creds)
+    spreadsheets = client.open(sheet)
+    details = {}
+    for spreadsheet in spreadsheets:
+        details[spreadsheet.title] = spreadsheet.get_all_values()
+        # print(spreadsheet.get_all_values())
+        print(spreadsheet.title)
+    with open('details.json', 'w') as f:
+        json.dump(details, f)
+    
+    return details
+
 def gsheet_load(array, sheet, tab):
     scope = [
     'https://www.googleapis.com/auth/drive',
@@ -129,6 +148,82 @@ def make_snap(mat_1, mat_2, mat_3, sheet, tab):
     # pprint(master_mat)
     gsheet_load(master_mat, sheet, tab)
 
+def format_daily_details(date, matrix, obi_wan_kenobi):
+    pos_1 = matrix[1].index("")
+    pos_2 = matrix[1].index("", pos_1+1)
+
+    obsts = []
+    gemuses = []
+    potatoes = []
+
+    for i in range(1,len(matrix)):
+        for j in range(0, pos_1):
+            row = []
+            row.append(matrix[i][j])
+            obsts.append(row)
+        for j in range(pos_1+1, pos_2):
+            row = []
+            row.append(matrix[i][j])
+            gemuses.append(row)
+        for j in range(pos_2+1,len(matrix[i])):
+            row = []
+            row.append(matrix[i][j])
+            potatoes.append(row)
+
+    date = datetime.strptime(date, '%m-%d').strftime('%d%b') 
+    
+    if date in obi_wan_kenobi["obst"][0]:
+        date_index = obi_wan_kenobi["obst"].index(date)
+    else:
+        obi_wan_kenobi["obst"][0].append(date)
+        for i in range(1,obi_wan_kenobi["obst"]):
+            obi_wan_kenobi["obst"][i].append('')
+    product = ""
+    for obst in obsts:
+        if len(list(filter(None, obst))):
+            product = obst[0] 
+            continue
+        heading = product + ''.join(list(filter(None, obst[0:4])))
+        mittel = obst[-1].split()[0]
+        for obi in obi_wan_kenobi["obst"]:
+            if obi[0] == heading:
+                obi[date_index] = mittel
+    product = ""
+    for gemuse in gemuses:
+        if len(list(filter(None, gemuse))):
+            product = gemuse[0] 
+            continue
+        heading = product + ''.join(list(filter(None, gemuse[0:4])))
+        mittel = gemuse[-1].split()[0]
+        for obi in obi_wan_kenobi["gemuse"]:
+            if obi[0] == heading:
+                obi[date_index] = mittel
+    product = ""
+    for potato in potatoes:
+        if len(list(filter(None, potato))):
+            product = potato[0] 
+            continue
+        heading = product + ''.join(list(filter(None, potato[0:4])))
+        mittel = potato[-1].split()[0]
+        for obi in obi_wan_kenobi[["potatoes"]]:
+            if obi[0] == heading:
+                obi[date_index] = mittel
+
+    return obi_wan_kenobi
+    
+def add_daily():
+    with open("details.json") as f:
+        details = json.load(f)
+    obi_wan_kenobi = {}
+    obi_wan_kenobi["obst"] = []
+    obi_wan_kenobi["gemuse"] = []
+    obi_wan_kenobi["potatoes"] = []
+    for key_detail, value_detail in details.items():
+        if re.match(r'\d\d-\d\d',key_detail):
+            obi_wan_kenobi = format_daily_details(key_detail, value_detail, obi_wan_kenobi)
+
+
+
 def daily_driver():
     cookies = login()
     table_1 = "https://www.ami-informiert.de/ami-onlinedienste/markt-aktuell-obst-und-gemuese/preise/grossmaerkte?purg=1%2C2%2C6%2C20%2C25%2C30%2C35%2C36%2C37%2C50%2C51%2C70%2C71%2C75%2C76%2C78%2C90%2C91%2C92%2C93%2C99%2"
@@ -180,6 +275,7 @@ def driver_verbrauch():
 
 def driver():
     # weekly_driver()
-    driver_verbrauch()
+    # driver_verbrauch()
+    get_sheet('AMI_Snaps_PG')
 
     
