f712f586bbf2017db30c6c5519079de104583721
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-06-08 18:44:49 +0530

    schedule added

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
 from helper import driver
+import time
+import schedule
 
 if __name__ == "__main__":
-    driver()
+    for i in ["07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"]:
+        schedule.every().monday.at(i).do(driver)
+        schedule.every().tuesday.at(i).do(driver)
+        schedule.every().wednesday.at(i).do(driver)
+        schedule.every().thursday.at(i).do(driver)
+        schedule.every().friday.at(i).do(driver)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(30)
